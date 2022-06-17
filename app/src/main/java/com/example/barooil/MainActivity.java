1d240a3acad7e2ec560faeb921e531782ea2b3f5
mincube12 <mincube12@naver.com>
2022-06-17 19:38:31 +0900

    start

---
+++ app/src/main/java/com/example/barooil/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.barooil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button mapbutton = (Button) findViewById(R.id.changemap);
+        mapbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Map.class);
+                startActivity(intent);
+            }
+        });
+
+        Button favoritebutton = (Button) findViewById(R.id.changefavorite);
+        favoritebutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view1) {
+                Intent intent1 = new Intent(getApplicationContext(), Favorite.class);
+                startActivity(intent1);
+            }
+        });
+    }
+}
